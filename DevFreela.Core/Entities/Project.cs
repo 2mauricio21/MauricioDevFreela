52c2f11b096739fffb19431ae65f53a04110a565
Mauricio Souza <mauricio.salso@hotmail.com>
2025-07-26 14:38:39 -0300

    feat: Criação base das classes do Core
    - Foi criado a estutura base do projeto, com as entidades modelo seguindo o padrão do Clean Architecture

---
+++ DevFreela.Core/Entities/Project.cs
@@ -0,0 +1,34 @@
+﻿using DevFreela.Core.Enums;
+using System;
+using System.Collections.Generic;
+
+namespace DevFreela.Core.Entities
+{
+    public class Project : BaseEntity
+    {
+        public Project(string title, string description, int idClient, int idFreeLancer, decimal totalConst)
+        {
+            Title = title;
+            Description = description;
+            IdClient = idClient;
+            IdFreeLancer = idFreeLancer;
+            TotalConst = totalConst;
+
+            CreatedAt = DateTime.Now;
+            Status = ProjectStatusEnum.Created;
+            Comments = new List<ProjectComment>();
+
+        }
+
+        public string Title { get; private set; }
+        public string Description { get; private set; }
+        public int IdClient { get; private set; }
+        public int IdFreeLancer { get; private set; }
+        public decimal TotalConst { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+        public DateTime? StartAt { get; private set; }
+        public DateTime? FinishedAd { get; private set; }
+        public ProjectStatusEnum Status { get; private set; }
+        public List<ProjectComment> Comments { get; set; }
+    }
+}
---
+++ DevFreela.Core/Entities/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DevFreela.Core.Entities
+{
+    public class User : BaseEntity
+    {
+        public User(string fullName, string email, DateTime birthName)
+        {
+            FullName = fullName;
+            Email = email;
+            BirthName = birthName;
+            CreatedAt = DateTime.Now;
+            Active = true;
+            Skills = new List<UserSkill>();
+            OwnerProjects = new List<Project>();
+            FreeLanceProjects = new List<Project>();
+        }
+
+        public string FullName { get; private set; }
+        public string Email { get; private set; }
+        public DateTime BirthName { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+        public bool Active { get; set; }
+
+        public List<UserSkill> Skills { get; private set; }
+        public List<Project> OwnerProjects { get; private set; }
+        public List<Project> FreeLanceProjects { get; private set; }
+    }
+}
