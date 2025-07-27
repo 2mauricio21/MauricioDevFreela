99de483dbc69670d23a84298eb3e92f1be865de4
Mauricio Souza <mauricio.salso@hotmail.com>
2025-07-27 19:16:43 -0300

    feat: implementação base dos serviços do end point de ProjectController

---
+++ DevFreela.Application/Services/Interfaces/IUserService.cs
@@ -0,0 +1,13 @@
+﻿using DevFreela.Core.Entities;
+using System.Collections.Generic;
+
+namespace DevFreela.Application.Services.Interfaces
+{
+    public interface IUserService
+    {
+        List<User> GetAll();
+
+        User GetById(int id);
+
+    }
+}
