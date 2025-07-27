5fb95bef6828c44770b4ebafaf36e861861c4531
Mauricio Souza <mauricio.salso@hotmail.com>
2025-07-27 17:34:51 -0300

    feat: Implementação base da service de Project.

--- DevFreela.Core/Entities/Project.cs
+++ DevFreela.Core/Entities/Project.cs
@@ -26,9 +26,40 @@ namespace DevFreela.Core.Entities
         public int IdFreeLancer { get; private set; }
         public decimal TotalConst { get; private set; }
         public DateTime CreatedAt { get; private set; }
-        public DateTime? StartAt { get; private set; }
+        public DateTime? StartedAt { get; private set; }
         public DateTime? FinishedAd { get; private set; }
         public ProjectStatusEnum Status { get; private set; }
         public List<ProjectComment> Comments { get; set; }
+
+        public void Cancel()
+        {
+            if (Status == ProjectStatusEnum.InProgress)
+            {
+                Status = ProjectStatusEnum.Cancelled;
+            }
+        }
+        public void Start()
+        {
+            if (Status == ProjectStatusEnum.Created)
+            {
+                Status = ProjectStatusEnum.InProgress;
+                StartedAt = DateTime.Now;
+            }
+        }
+        public void Finish()
+        {
+            if (Status == ProjectStatusEnum.InProgress)
+            {
+                Status = ProjectStatusEnum.Finished;
+                FinishedAd = DateTime.Now;
+            }
+        }
+        // temporario
+        public void Update(string title, string description, decimal totalCost)
+        {
+            Title = title;
+            Description = description;
+            TotalConst = totalCost;
+        }
     }
 }
