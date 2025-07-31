ae24dd013f66968384e9fd74b03a2f6999c7bf09
Mauricio Souza <mauricio.salso@hotmail.com>
2025-07-31 03:04:30 -0300

    feat: implementando o metodo CreateUserComment

--- DevFreela.API/Controllers/UsersController.cs
+++ DevFreela.API/Controllers/UsersController.cs
@@ -1,11 +1,19 @@
 ﻿using DevFreela.API.Models;
+using DevFreela.Application.Commands.CreateUser;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace DevFreela.API.Controllers
 {
     [Route("api/users")]
     public class UsersController : ControllerBase
     {
+        private readonly IMediator _mediator;
+        public UsersController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
         [HttpGet("{id}")]
         public IActionResult GetById(int id)
         {
@@ -13,9 +21,11 @@ namespace DevFreela.API.Controllers
         }
 
         [HttpPost]
-        public IActionResult Post([FromBody] CreateUserModel createUserModel)
+        public async Task<IActionResult> Post([FromBody] CreateUserCommand command)
         {
-            return CreatedAtAction(nameof(GetById), new { id = 1 }, createUserModel);
+            //return CreatedAtAction(nameof(GetById), new { id = 1 }, createUserModel);
+            var id = await _mediator.Send(command);
+            return CreatedAtAction(nameof(GetById), new { id }, command);
         }
 
         [HttpPut("{id}/login")]
