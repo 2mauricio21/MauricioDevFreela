d3c94ea06e1330954c75cf88fe3bdce7210a7057
Mauricio Souza <mauricio.salso@hotmail.com>
2025-09-22 08:42:07 -0300

    refact: Refatora acesso a projetos para uso de padrão Repository,
    - Substitui o uso do `DevFreelaDbContext` na classe `GetAllProjectsQueryHandler` por um repositório de projetos (`IProjectRepository`) melhorando a separação de preocupações e a testabilidade.
    - Adiciona a assinatura do método `GetAll` na interface `IProjectRepository` e implementa a lógica de acesso a dados no novo arquivo `ProjectRepository.cs`.
    - Remove elementos desnecessários do arquivo `DevFreela.Infrastructure.csproj`.

---
+++ DevFreela.Infrastructure/Repositories/ProjectRepository.cs
@@ -0,0 +1,23 @@
+﻿using DevFreela.Core.Entities;
+using DevFreela.Core.Repositories;
+using DevFreela.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DevFreela.Infrastructure.Repositories
+{
+    public class ProjectRepository : IProjectRepository
+    {
+        private readonly DevFreelaDbContext _dbContext;
+        public ProjectRepository(DevFreelaDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<List<Project>> GetAll()
+        {
+            return await _dbContext.Projects.ToListAsync();
+        }
+    }
+}
