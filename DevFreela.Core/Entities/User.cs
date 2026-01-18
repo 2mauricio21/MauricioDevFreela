d1c87e39de852635691db5e3f530eae64978a603
Mauricio Souza <mauricio.salso@hotmail.com>
2026-01-18 20:07:58 -0300

    feat: Suporte à autenticação JWT e novos campos em User
    Adiciona AuthService e interface para geração de tokens JWT.
    Inclui configurações de JWT em appsettings.json.
    Adiciona campos Password e Role à entidade User e ajusta construtores e comandos.
    Cria migração para atualizar o banco de dados com os novos campos e relacionamentos.
    Atualiza DevFreelaDbContextModelSnapshot conforme o novo modelo.

--- DevFreela.Core/Entities/User.cs
+++ DevFreela.Core/Entities/User.cs
@@ -5,13 +5,16 @@ namespace DevFreela.Core.Entities
 {
     public class User : BaseEntity
     {
-        public User(string fullName, string email, DateTime birthName)
+        public User(string fullName, string email, DateTime birthName, string password, string role)
         {
             FullName = fullName;
             Email = email;
             BirthName = birthName;
             CreatedAt = DateTime.Now;
             Active = true;
+            Password = password;
+            Role = role;
+            // Initialize collections
             Skills = new List<UserSkill>();
             OwnerProjects = new List<Project>();
             FreelanceProjects = new List<Project>();
@@ -22,6 +25,8 @@ namespace DevFreela.Core.Entities
         public DateTime BirthName { get; private set; }
         public DateTime CreatedAt { get; private set; }
         public bool Active { get; set; }
+        public string Password { get; private set; }
+        public string Role { get; private set; }
         public List<UserSkill> Skills { get; private set; }
         public List<Project> OwnerProjects { get; private set; }
         public List<Project> FreelanceProjects { get; private set; }
---
+++ DevFreela.Core/Services/IAuthService.cs
@@ -0,0 +1,7 @@
+﻿namespace DevFreela.Core.Services
+{
+    public interface IAuthService
+    {
+        string GenerateJwToken(string email, string role);
+    }
+}
